821126a599c0445a869475c9494570af9cf69bf9
Vidya Thakur <Vidya@Chandravijays-MacBook-Air.local>
2016-11-04 17:01:50 -0700

    ALl the Web Dev

---
+++ All the other projects/Conway_Game/Conway.js
@@ -0,0 +1,104 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+     <title>ChakIT</title>
+    <style>
+        .form-section {
+        padding-top:40px;
+        }
+    </style>
+</head>
+<body>
+    <!--Navbar-->
+    <nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
+        <div class="contianer">
+            <div class="nav-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="#" class="navbar-brand">LOGO</a>
+                <div class="collapse navbar-collapse" id="navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Feedback</a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Support</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </nav>
+    <div class="jumbotron text-center">
+        <div class="container">
+            <h1>ChalkIT</h1>
+            <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin.</p>
+        <div class="btn=grp">
+            <a href="#" class = "btn btn-md btn-warning">Download it for IOS</a>
+            <a href="#" class="btn btn-md btn-warning">Download it for Android</a>
+            <a href="#" class="btn btn-md btn-warning">Spread the word</a>
+        </div>
+    </div>
+</div>
+    <div class="container">
+        <section>
+            <div class ="page-header" id="#Feedback">
+            <h2>Feedback: Let's see what people have to say about the app</h2>
+            </div><!--Page header-->
+            <div class="col-lg-4">
+            <blockquote>
+                <p>
+                    Nulla dapibus, diam sed gravida lobortis, diam lectus cursus ante, ac tristique arcu elit facilisis dui. 
+                    Quisque ut erat elit. Nulla ligula nulla, auctor at leo nec, facilisis suscipit odio. Curabitur sagittis ut massa quis bibendum. 
+                    Ut in accumsan nulla. Praesent lobortis sollicitudin arcu. Nunc semper fermentum sollicitudin.
+                </p>
+                <footer>Dummy Name</footer>
+            </blockquote>
+            </div><!--col1-->
+            <div class="col-lg-4">
+            <blockquote>
+                <p>
+                    Aliquam dignissim a urna a maximus. Aliquam non condimentum augue, scelerisque bibendum felis. Cras sit amet laoreet lectus. Fusce pharetra nibh sit amet felis rutrum finibus. 
+                    Sed quis neque porttitor ipsum placerat tempor. Etiam facilisis ultricies ante non lacinia. 
+                </p>
+                <footer>Dummy Name2</footer>
+            </blockquote>
+            </div><!--col2-->
+            <div class="col-lg-4">
+                <blockquote>
+                    <p>
+                        Nam porttitor laoreet metus at ultricies. Praesent vel nisi sem. Donec non risus mi. Donec ut orci efficitur, pellentesque mi ut, consequat tortor. 
+                        Sed accumsan pellentesque sem, nec volutpat orci fringilla sit amet. Cras tincidunt dui sed ligula mattis, nec feugiat quam luctus. 
+                        Ut quis quam molestie erat mattis varius et sed felis.
+                    </p>
+                    <footer>Dummy Name3</footer>
+                </blockquote>
+            </div><!--col3-->
+            <button class="btn btn-default">Click here to read more reviews</button>
+        </section>
+    </div><!--Feedback container-->
+    <div class="form-section">
+        <div class="well text-center">
+            <div class="container">
+                <h1>Subscribe to recieve updates</h1>
+                <form action="">
+                    <div class="form-inline form-group text-left">
+                        <label for="Name">Full Name:  </label>
+                        <input type="text" id="name" class="form-control" />
+                        <label for="Email"> Email: </label>
+                        <input type="email" id="email" class="form-control"/>
+                    </div><!--form group-->
+                </form>
+            </div><!--well-->
+        </div><!--container-->
+    </div><!--form action-->
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
